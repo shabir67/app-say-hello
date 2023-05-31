d7129feff038d6d356a03a3265ac098a994c127f
Muhammad Shobir Abdussyakur <muhammadshobirabdussyakur@Macintoshs-MacBook-Pro.local>
2023-05-31 12:54:42 +0700

    implemented spaces after words "hello"

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/shabir67/app-say-hello
 go 1.20
 
 require github.com/shabir67/go-say-hello v1.0.1
+
+require github.com/shabir67/go-say-hello/v2 v2.0.1
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/shabir67/go-say-hello"
+	gosayhello "github.com/shabir67/go-say-hello/v2"
 )
 
 func main() {
-	hello := gosayhello.SayHello(" Joko")
+	hello := gosayhello.SayHello("Joko")
 	fmt.Println(hello)
 }
